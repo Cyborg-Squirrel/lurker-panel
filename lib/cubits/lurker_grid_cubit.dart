c1e124363d74242457c19730332bc289254f0cd4
Cyborg-Squirrel <ashlybelka@gmail.com>
2024-01-04 10:48:11 -0600

    Added background image, removed unused import, added js package to pubspec for future web support

--- lib/cubits/lurker_grid_cubit.dart
+++ lib/cubits/lurker_grid_cubit.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lurker_panel/config/lurker_panel_config.dart';
 import 'package:lurker_panel/model/lurker_model.dart';
--- lib/widgets/lurker_grid_widget.dart
+++ lib/widgets/lurker_grid_widget.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -23,89 +24,113 @@ class LurkerGridWidget extends StatelessWidget {
       //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       //   title: const Text(title),
       // ),
-      body: StreamBuilder(
-        stream: cubit.stream,
-        initialData: cubit.state,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final state = snapshot.data;
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: StreamBuilder(
+          stream: cubit.stream,
+          initialData: cubit.state,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final state = snapshot.data;
 
-            final width = MediaQuery.of(context).size.width;
-            final height = MediaQuery.of(context).size.height;
-            final ratio = width / height;
-            int crossAxisCount = 5;
-            if (ratio > 6) {
-              crossAxisCount = 8;
-            } else if (ratio > 1.4) {
-              crossAxisCount = 5;
-            } else if (ratio > 1 && ratio <= 1.4) {
-              crossAxisCount = 3;
-            } else if (ratio > .5 && ratio < 1) {
-              crossAxisCount = 2;
-            } else {
-              crossAxisCount = 1;
+              final width = MediaQuery.of(context).size.width;
+              final height = MediaQuery.of(context).size.height;
+              final ratio = width / height;
+              int crossAxisCount = 5;
+              if (ratio > 6) {
+                crossAxisCount = 8;
+              } else if (ratio > 1.4) {
+                crossAxisCount = 5;
+              } else if (ratio > 1 && ratio <= 1.4) {
+                crossAxisCount = 3;
+              } else if (ratio > .5 && ratio < 1) {
+                crossAxisCount = 2;
+              } else {
+                crossAxisCount = 1;
+              }
+              print('ratio $ratio');
+              print('crossAxisCount $crossAxisCount');
+
+              if (state is LurkerGridState) {
+                return Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: GridView.count(
+                    crossAxisCount: crossAxisCount,
+                    children: List.generate(state.lurkerList.length, (index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: getGridItem(
+                            context, index, state.lurkerList[index]),
+                      );
+                    }),
+                  ),
+                );
+              }
             }
-            print('ratio $ratio');
-            print('crossAxisCount $crossAxisCount');
 
-            if (state is LurkerGridState) {
-              return GridView.count(
-                crossAxisCount: crossAxisCount,
-                children: List.generate(state.lurkerList.length, (index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: getGridItem(context, index, state.lurkerList[index]),
-                  );
-                }),
-              );
-            }
-          }
-
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
       ),
     );
   }
 }
 
 Widget getGridItem(BuildContext context, int index, LurkerModel lurkerModel) {
+  var displayName = lurkerModel.name;
+  if (displayName.length > 13) {
+    displayName = '${displayName.substring(0, 10)}...';
+  }
   return GestureDetector(
     onTap: () {
       getIt<LurkerGridCubit>().unlurk(lurkerModel);
     },
     child: Card(
+      elevation: 0,
+      color: Colors.transparent,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(32),
-              child: SizedBox.fromSize(
-                size: const Size.fromRadius(24),
-                child: Image.network(
-                  lurkerModel.profileImageUrl!,
-                  fit: BoxFit.cover,
-                ),
+            child: Center(
+              child: CircleAvatar(
+                radius: 300,
+                backgroundImage: NetworkImage(lurkerModel.profileImageUrl!),
               ),
             ),
-            // child: Center(
-            //   child: CircleAvatar(
-            // radius: 300,
-            // backgroundImage: NetworkImage(lurkerModel.profileImageUrl!),
-            //   ),
-            // ),
           ),
           Center(
             child: Container(
-              padding: const EdgeInsets.all(4.0),
-              child: Text(
-                lurkerModel.name,
-                style:
-                    const TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-              ),
-            ),
+                padding: const EdgeInsets.all(4.0),
+                child: Stack(
+                  children: <Widget>[
+                    Text(
+                      displayName,
+                      style: TextStyle(
+                        fontSize: 24,
+                        foreground: Paint()
+                          ..style = PaintingStyle.stroke
+                          ..strokeWidth = 6
+                          ..color = Colors.black,
+                      ),
+                    ),
+                    // Solid text as fill.
+                    Text(
+                      displayName,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                )),
           ),
         ],
       ),
